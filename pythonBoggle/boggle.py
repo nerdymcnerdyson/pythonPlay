57e18b959e0f42a56a3c2b35ee006bb60aa07499
Michael Byrd <nerdymcnerdyson@gmail.com>
2015-09-01 20:49:54 -0700

    initial commit

---
+++ pythonBoggle/boggle.py
@@ -0,0 +1,30 @@
+
+
+
+class BoggleDie:
+    def __init__(self, letters):
+        self.letters = []
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['i','f','e','h','e','y'])
+        self.letters.append(['d','e','n','o','w','s'])
+        self.letters.append(['u','t','o','k','n','d'])
+        self.letters.append(['h','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['r','i','f','o','b','x'])
+        
+
+class Boggle:
+    
+    def __init__(self, n):
+        self.n = n
+        
+
+
+    def createBoard():
+        
---
+++ pythonTrie/trie.py
@@ -0,0 +1,126 @@
+class TrieNode:
+    def __init__(self, letter, parent):
+        self.letter = letter
+        self.parent = parent
+        self.id = None
+        self.children = {}
+        self.kids = 0
+    def processString(self, stringIn, stringId):
+        if len(stringIn) == 0:
+            self.id = stringId
+            return
+        self.children.setdefault(stringIn[0],TrieNode(stringIn[0], self))
+        self.children[stringIn[0]].processString(stringIn[1:], stringId)
+        self.kids += 1
+
+    def getStringId(self, stringIn):
+        if len(stringIn) == 0:
+            return self.id
+        childNode = self.children.get(stringIn[0], None)
+        if not childNode:
+            return None
+        return childNode.getStringId(stringIn[1:])
+
+    def printReport(self,depth, startDepth):
+        print self.letter
+        indent = '\t' * (startDepth - depth)
+        for letter in self.children.keys():
+            print indent,letter, len(self.children[letter].children)
+        if depth - 1 > 0:
+            for child in self.children.values():
+                child.printReport(depth-1, startDepth)
+
+
+class Trie:
+    def __init__(self):
+        self.stringsIndexed = 0
+        self.root = None
+
+        
+    def processString(self, stringIn, stringId):
+        if len(stringIn) == 0:
+            return 
+        if self.root == None:
+            self.root = TrieNode('*', None)
+        self.root.processString(stringIn,stringId)
+        self.stringsIndexed += 1
+
+    def getIdForString(self, stringIn):
+        if self.root == None:
+            return None
+        return self.root.getStringId(stringIn)
+
+
+    def printRootReport(self, depth, startDepth):
+        if not self.root:
+            return
+        self.root.printReport(depth, startDepth)
+        
+    
+
+
+def getMysqlDatabaseEmails():
+    import MySQLdb
+
+    db = MySQLdb.connect(host="", # your host, usually localhost
+                     user="root", # your username
+                      passwd="stupidpandabear714", # your password
+                      db="AMUsers") # name of the data base
+    
+    # you must create a Cursor object. It will let
+    #  you execute all the queries you need
+    cur = db.cursor() 
+
+    # Use all the SQL you like
+    cur.execute("select email,pnum from aminno_member_email where char_length(email) > 0 limit 200000;")
+
+    return cur
+
+
+
+    
+
+
+
+def main():
+    myTrie = Trie()
+
+    import time
+
+    ts = time.time()
+
+    DO_EMAIL = False
+    DO_DICT  = True
+
+    if DO_EMAIL:
+        emailList = getMysqlDatabaseEmails()
+        print 'getting email list took', (time.time() - ts)
+        ts = time.time()
+    
+        for email in emailList:
+            myTrie.processString(email[0].lstrip().rstrip(), email[1])
+        print 'processing records took', (time.time() - ts)
+
+    elif DO_DICT:
+        dictFile = open('enable1.txt','r')
+        count = 1
+        ts = time.time()
+        for line in dictFile:
+            myTrie.processString(line.lstrip().rstrip(), count)
+            count += 1
+        print 'processing records took', (time.time() - ts)
+
+        
+            
+
+    print 'this trie has processed', myTrie.stringsIndexed, 'strings'
+        
+    print 'getting ID for emailAddress bigdaddy200698@yahoo.com', myTrie.getIdForString('bigdaddy200698@yahoo.com')
+    print 'getting ID for emailAddress pizza', myTrie.getIdForString('pizza')
+
+
+    myTrie.printRootReport(2,2)
+    
+        
+if __name__ == '__main__':
+    main()
