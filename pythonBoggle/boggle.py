5ae958c60fec864f743553dc7750245fda98cb88
Michael Byrd <michael.byrd@bigfishgames.com>
2015-09-05 14:44:33 -0700

    solve a boggle board using the trie nothing prettied up CR: nobody

--- pythonBoggle/boggle.py
+++ pythonBoggle/boggle.py
@@ -1,30 +1,145 @@
+import numpy
+import pythonTrie
+import time
+#00 01 02 03
+#04 05 06 07
+#08 09 10 11
+#12 13 14 15
 
 
 
 class BoggleDie:
-    def __init__(self, letters):
+    def __init__(self):
         self.letters = []
         self.letters.append(['r','i','f','o','b','x'])
         self.letters.append(['i','f','e','h','e','y'])
         self.letters.append(['d','e','n','o','w','s'])
         self.letters.append(['u','t','o','k','n','d'])
-        self.letters.append(['h','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
-        self.letters.append(['r','i','f','o','b','x'])
+        self.letters.append(['h','m','s','r','a','o'])
+        self.letters.append(['l','u','p','e','t','s'])
+        self.letters.append(['a','c','i','t','o','a'])
+        self.letters.append(['y','l','g','k','u','e'])
+        self.letters.append(['qu','b','m','j','o','a'])
+        self.letters.append(['e','h','i','s','p','n'])
+        self.letters.append(['v','e','t','i','g','n'])
+        self.letters.append(['b','a','l','i','y','t'])
+        self.letters.append(['e','z','a','v','n','d'])
+        self.letters.append(['r','a','l','e','s','c'])
+        self.letters.append(['u','w','i','l','r','g'])
+        self.letters.append(['p','a','c','e','m','d'])
         
+
+class BoggleBoard:
+    def __init__(self, N, dictTrie):
+        #for the first N letters, use the approved die randomly.. after.. grab another set of die Iguess?
+
+        #shuffle die, put them on random face 
+        letters = []
+        dice = BoggleDie()
+        diceList = dice.letters
+        #numpy.random.shuffle(diceList)
+        
+
+        while len(letters) < N*N:
+            numpy.random.shuffle(diceList)
+            for die in diceList:
+                numpy.random.shuffle(die)
+                letters.append(die[0])
+                if len(letters) >= N*N:
+                    break
+        #print letters
+        self.letters = letters
+        self.rows = N
+        self.dictTrie =  dictTrie
+        self.foundWords = {}
+
+
+    def getNeighbors(self,n):
+        row = n / self.rows
+        col = n % self.rows
+        #print row,col
+        neighbors = []
+        dirs = [-1,0,1]
+        for dx in dirs:
+            for dy in dirs:
+                newRow = row + dy
+                newCol = col + dx
+                if newRow >= 0 and newRow < self.rows and newCol >= 0 and newCol < self.rows and (newRow != row or newCol != col):
+                    #print 'adding', newCol, newRow
+                    neighbors.append((newRow*self.rows) + newCol)
+                    #print neighbors
+        return neighbors
+
+    def walkBoard(self, square,word, used):
+        #print 'walking board for square:', square, 'with word', word, 'with used:', used
+        myletter = self.letters[square]
+        newWord = word+myletter
+        newUsed = used+[square]
+        #given this word, a) is it a word? and b) are there words with this root?
+        wordId,remainingWords = self.dictTrie.getIdAndRootCountForString(newWord)
+        #check word and root
+
+        if wordId > 0 and len(newWord) > 2: 
+            self.foundWords[newWord] = newUsed
+        if remainingWords < 1:
+            #print 'short cutting for root:', newWord, 'with remainingWords', remainingWords
+            return
+
+        
+
+
+
+        neighbors = self.getNeighbors(square)
+        for neighbor in neighbors:
+            if neighbor not in used:
+                self.walkBoard(neighbor, newWord,newUsed)
+
+
+
 
 class Boggle:
     
-    def __init__(self, n):
+    def __init__(self, n, dictTrie):
         self.n = n
-        
+        self.board = None
+        self.dictTrie = dictTrie
 
 
-    def createBoard():
+    def createBoard(self):
         
+        self.board = BoggleBoard(self.n, self.dictTrie)
+
+    def walkBoard(self):
+        for i in xrange(self.n*self.n):
+            self.board.walkBoard(i,"",[])
+        allWords = self.board.foundWords.keys()
+        allWords.sort(key = len)
+        print 'found', len(allWords), 'words'
+
+
+        for word in allWords[-10:]:
+            print word, self.board.foundWords[word]
+
+    def prettyPrintBoard(self):
+        if not self.board:
+            return
+        for i in xrange(self.n):
+            for j in xrange(self.n):
+                letter = self.board.letters[i*self.n+j]
+                if len(letter) == 1:
+                    letter += " "
+                print letter,
+            print ''
+
+def main():
+
+    myTrie = pythonTrie.trie.Trie()
+    myTrie.processDict()
+
+    boggleGame = Boggle(20, myTrie)
+    boggleGame.createBoard()
+    boggleGame.walkBoard()
+    boggleGame.prettyPrintBoard()
+
+if __name__ == '__main__':
+    main()
--- pythonTrie/trie.py
+++ pythonTrie/trie.py
@@ -1,3 +1,4 @@
+import time
 class TrieNode:
     def __init__(self, letter, parent):
         self.letter = letter
@@ -21,6 +22,14 @@ class TrieNode:
             return None
         return childNode.getStringId(stringIn[1:])
 
+    def getIdAndKidsForString(self, stringIn):
+        if len(stringIn) == 0:
+            return [self.id, self.kids]
+        childNode = self.children.get(stringIn[0], None)
+        if not childNode:
+            return [-1, 0]
+        return childNode.getIdAndKidsForString(stringIn[1:])
+
     def printReport(self,depth, startDepth):
         print self.letter
         indent = '\t' * (startDepth - depth)
@@ -41,14 +50,21 @@ class Trie:
         if len(stringIn) == 0:
             return 
         if self.root == None:
-            self.root = TrieNode('*', None)
+            self.root = TrieNode('*', 0)
         self.root.processString(stringIn,stringId)
         self.stringsIndexed += 1
 
     def getIdForString(self, stringIn):
         if self.root == None:
-            return None
+            return -1
         return self.root.getStringId(stringIn)
+
+
+    def getIdAndRootCountForString(self, StringIn):
+        if self.root == None:
+            return [-1,-1]
+        return self.root.getIdAndKidsForString(StringIn)
+
 
 
     def printRootReport(self, depth, startDepth):
@@ -56,6 +72,14 @@ class Trie:
             return
         self.root.printReport(depth, startDepth)
         
+    def processDict(self):
+        dictFile = open('pythonTrie/enable1.txt','r')
+        count = 1
+        ts = time.time()
+        for line in dictFile:
+            self.processString(line.lstrip().rstrip(), count)
+            count += 1
+        print 'processing records took', (time.time() - ts)
     
 
 
@@ -82,45 +106,45 @@ def getMysqlDatabaseEmails():
 
 
 
-def main():
-    myTrie = Trie()
+# def main():
+#     myTrie = Trie()
 
-    import time
+#     import time
 
-    ts = time.time()
+#     ts = time.time()
 
-    DO_EMAIL = False
-    DO_DICT  = True
+#     DO_EMAIL = False
+#     DO_DICT  = True
 
-    if DO_EMAIL:
-        emailList = getMysqlDatabaseEmails()
-        print 'getting email list took', (time.time() - ts)
-        ts = time.time()
+#     if DO_EMAIL:
+#         emailList = getMysqlDatabaseEmails()
+#         print 'getting email list took', (time.time() - ts)
+#         ts = time.time()
     
-        for email in emailList:
-            myTrie.processString(email[0].lstrip().rstrip(), email[1])
-        print 'processing records took', (time.time() - ts)
+#         for email in emailList:
+#             myTrie.processString(email[0].lstrip().rstrip(), email[1])
+#         print 'processing records took', (time.time() - ts)
 
-    elif DO_DICT:
-        dictFile = open('enable1.txt','r')
-        count = 1
-        ts = time.time()
-        for line in dictFile:
-            myTrie.processString(line.lstrip().rstrip(), count)
-            count += 1
-        print 'processing records took', (time.time() - ts)
+#     elif DO_DICT:
+#         dictFile = open('enable1.txt','r')
+#         count = 1
+#         ts = time.time()
+#         for line in dictFile:
+#             myTrie.processString(line.lstrip().rstrip(), count)
+#             count += 1
+#         print 'processing records took', (time.time() - ts)
 
         
             
 
-    print 'this trie has processed', myTrie.stringsIndexed, 'strings'
+#     print 'this trie has processed', myTrie.stringsIndexed, 'strings'
         
-    print 'getting ID for emailAddress bigdaddy200698@yahoo.com', myTrie.getIdForString('bigdaddy200698@yahoo.com')
-    print 'getting ID for emailAddress pizza', myTrie.getIdForString('pizza')
+#     print 'getting ID for emailAddress bigdaddy200698@yahoo.com', myTrie.getIdForString('bigdaddy200698@yahoo.com')
+#     print 'getting ID for emailAddress pizza', myTrie.getIdForString('pizza')
 
 
-    myTrie.printRootReport(2,2)
+#     myTrie.printRootReport(2,2)
     
         
-if __name__ == '__main__':
-    main()
+# if __name__ == '__main__':
+#     main()
